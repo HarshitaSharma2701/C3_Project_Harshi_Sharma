9c32cc9cee4955f554318ce6db391340f0f7d83c
Harshita Sharma <HarshitaSharma2701>
2021-03-01 20:39:10 +0530

    Part 3: Failing test case

--- Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
@@ -32,6 +32,17 @@ class RestaurantServiceTest {
 
 
     }
+    @Test
+    public void searching_for_existing_restaurant_should_return_expected_restaurant_object_Failure_Scenario() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+        int existingRestaurant = service.findRestaurantByName().size();
+        service.getRestaurants("Amelie's cafe Wrong Input");
+        assertEquals("Amelia's Cafe", existingRestaurant); // returns False
+        assertNotEquals("Amelia's Cafe",existingRestaurant ); //returns true
+//        assertNotNull(existingRestaurant);
+
+
+    }
 
     //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
     @Test
@@ -63,6 +74,21 @@ class RestaurantServiceTest {
     }
 
     @Test
+    public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1_Failure_Scenario() throws restaurantNotFoundException {
+//        LocalTime openingTime = LocalTime.parse("10:30:00");
+//        LocalTime closingTime = LocalTime.parse("22:00:00");
+//        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+//        restaurant.addToMenu("Sweet corn soup",119);
+//        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.removeRestaurant("Amelia Cafe Wrong Input");
+        assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
+        assertNotEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
+
+    }
+
+    @Test
     public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException {
 //        LocalTime openingTime = LocalTime.parse("10:30:00");
 //        LocalTime closingTime = LocalTime.parse("22:00:00");
@@ -86,4 +112,19 @@ class RestaurantServiceTest {
         assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
     }
     //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    @Test
+    public void add_restaurant_should_increase_list_of_restaurants_size_by_1_Failure_Scenario(){
+//        LocalTime openingTime = LocalTime.parse("10:30:00");
+//        LocalTime closingTime = LocalTime.parse("22:00:00");
+//        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+//        restaurant.addToMenu("Sweet corn soup",119);
+//        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.addRestaurant("Invalid","Invalid",LocalTime.parse("26:00:00"),LocalTime.parse("27:00:00"));
+        System.out.println("Invalid Input")
+        assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size()); //return flase
+        assertNotEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size()); // Not Equals So return true
+    }
 }
--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -31,12 +31,34 @@ class RestaurantTest {
     }
 
     @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time_Failure_Scenario(){
+        //WRITE UNIT TEST CASE HERE
+//        restaurant = new Restaurant();
+
+        int isRestaurantOpened = restaurant.isRestaurantOpen().size();
+        restaurant.isRestaurantOpen("Invalid Input");
+        assertEquals("Invalid Input",restaurant.isRestaurantOpen().size());
+        System.out.println("Enter proper timing in proper format 23:00:00")
+
+    }
+
+    @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
 //        restaurant = new Restaurant();
         int isRestaurantClosed = restaurant.isRestaurantOpen().size();
         restaurant.isRestaurantOpen("Restaurant is Closed");
         assertEquals("Restaurant is Closed",restaurant.isRestaurantOpen().size());
+
+    }
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time_Failure_Scenario(){
+        //WRITE UNIT TEST CASE HERE
+//        restaurant = new Restaurant();
+        int isRestaurantClosed = restaurant.isRestaurantOpen().size();
+        restaurant.isRestaurantOpen("Out of Scope");
+        assertEquals("Out of Scope",restaurant.isRestaurantOpen().size());
+
 
     }
 
@@ -58,6 +80,21 @@ class RestaurantTest {
         assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
     @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1_Failure_Scenario(){
+        //Refactoring Repititive Code
+//        LocalTime openingTime = LocalTime.parse("10:30:00");
+//        LocalTime closingTime = LocalTime.parse("22:00:00");
+//        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+//        restaurant.addToMenu("Sweet corn soup",119);
+//        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+        System.out.println("Will decrease Instead of adding");
+    }
+
+    @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
         //Refactoring Repititive Code
 //        LocalTime openingTime = LocalTime.parse("10:30:00");
@@ -69,6 +106,22 @@ class RestaurantTest {
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
         assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+
+
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException_Failure_Scenario {
+        //Refactoring Repititive Code
+//        LocalTime openingTime = LocalTime.parse("10:30:00");
+//        LocalTime closingTime = LocalTime.parse("22:00:00");
+//        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+//        restaurant.addToMenu("Sweet corn soup",119);
+//        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+        System.out.println("Will Add instead of removal");
     }
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
